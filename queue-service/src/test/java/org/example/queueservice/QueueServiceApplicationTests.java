d01dff1066648fc1e5f8945bfdefc06a382bee0c
newrav1k <kritsky.academi@gmail.com>
2024-11-24 15:34:43 +0300

    Initial commit

---
+++ queue-service/src/test/java/org/example/queueservice/QueueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.queueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ schedule-service/src/test/java/org/example/scheduleservice/ScheduleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scheduleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
